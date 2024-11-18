c7ff607571cc761e5000660a5a8dff889d51ebb6
W Saahil <saahilgazi@gmail.com>
2024-11-18 07:53:45 +0000

    changed structure

--- toolkit/compath/qc/diagnosis.py
+++ toolkit/compath/qc/diagnosis.py
@@ -1,28 +1,21 @@
-import logging
-# Set up logging
-logging.basicConfig(
-    filename='qc_logs.txt',  # File where logs will be saved
-    level=logging.INFO,          # Log level (DEBUG, INFO, WARNING, ERROR, CRITICAL)
-    format='%(asctime)s - %(levelname)s - %(message)s',  # Log format
-)
-
 from pathlib import Path
+from typing import Optional, List
+
+from .qc_models_v1 import _TissueModelV1, _FocusModelV1, _FoldsModelV1, _PenModelV1
 
 from toolkit.compath.dataloading.slicer import Slicer
-
-from .qc_models_v1 import TissueModelV1, FocusModelV1, FoldsModelV1, PenModelV1
-
 from toolkit.geometry.shapely_tools import loads
 from toolkit.system.storage.load import h5
+
 
 class Diagnosis(Slicer):
     def __init__(
         self,
-        gpu_id=0,
-        device_type="gpu",
-        dataparallel=False,
-        data_loading_mode="cpu",
-        dataparallel_device_ids=None,
+        gpu_id: int =0,
+        device_type: str ="gpu",
+        dataparallel: bool =False,
+        data_loading_mode: str ="cpu",
+        dataparallel_device_ids: Optional[List[int]]=None,
     ):
         Slicer.__init__(
             self,
@@ -32,14 +25,8 @@ class Diagnosis(Slicer):
             data_loading_mode=data_loading_mode,
             dataparallel_device_ids=dataparallel_device_ids,
         )
-        self._qc_model_gpu_args = {
-            "gpu_id": gpu_id,
-            "device_type": device_type,
-            "dataparallel": dataparallel,
-            "dataparallel_device_ids": dataparallel_device_ids,
-        }
+        
         self._set_models()
-        self.slicer = slicer = Slicer()
         self._default_tissue_detector = "tissue_model_v1"
         self.tissue_detection_models = ["tissue_model_v1"]
         self.available_models = {
@@ -51,28 +38,31 @@ class Diagnosis(Slicer):
             ]
         }
 
+        self.model_name_mapping = {
+            "tissue_model_v1": _TissueModelV1,
+            "focus_model_v1": _FocusModelV1,
+            "folds_model_v1": _FoldsModelV1,
+            "pen_model_v1": _PenModelV1,
+        }
+
     def run_model_sequence(
         self,
         wsi_path: str,
         wsi_type: str,
-        model_run_sequence: list = ["tissue_model_v1"],
+        model_run_sequence: list[str] = ["tissue_model_v1"],
         **kwargs,
-    ):
+    )-> None:
         self.set_wsi(wsi_path=wsi_path, wsi_type=wsi_type)
-        logging.info(f"Initialised {wsi_type} object for {wsi_path.name}")
         self.tissue_path = Path(
             f"results/{self.wsi._wsi_path.stem}/qc/h5/{self._default_tissue_detector}.h5"
         )
-        
+
         if not self.tissue_path.exists():
             if set(model_run_sequence) & set(self.tissue_detection_models):
                 model_run_sequence = self._prioritize_tissue_model(model_run_sequence)
             else:
                 model_run_sequence.insert(0, self._default_tissue_detector)
-                logging.info(
-                    f"No tissue detector in the sequence, added {self._default_tissue_detector}"
-                )
-                
+
         for model_name in model_run_sequence:
             if Path(
                 f"results/{self.wsi._wsi_path.stem}/qc/h5/{model_name}.h5"
@@ -91,11 +81,10 @@ class Diagnosis(Slicer):
                 print(f"    {idx+1}. {available_model}")
             print("\n")
 
-    def _set_models(self):
-        self.tissue_model_v1 = TissueModelV1(**self._qc_model_gpu_args)
-        self.focus_model_v1 = FocusModelV1(**self._qc_model_gpu_args)
-        self.folds_model_v1 = FoldsModelV1(**self._qc_model_gpu_args)
-        self.pen_model_v1 = PenModelV1(**self._qc_model_gpu_args)
+    def set_model(self, model_name: str, model_args: dict = None):
+        model_class = self.model_mapping[model_name]
+        model_instance = model_class(**model_args)
+        setattr(self, f"{model_name}", model_instance)
 
     def _prioritize_tissue_model(self, model_run_sequence):
         model_set = set(model_run_sequence)
@@ -104,7 +93,6 @@ class Diagnosis(Slicer):
                 model_run_sequence.remove(model)
                 model_run_sequence.insert(0, model)
                 break
-        logging.info(f"Prioritized tissue model in sequence: {model_run_sequence}")
         return model_run_sequence
 
     def _run_model(
@@ -117,12 +105,10 @@ class Diagnosis(Slicer):
             tissue_wkt_dict = h5.load_wkt_dict(self.tissue_path)
             tissue_geom = loads(tissue_wkt_dict["combined"])
             self.set_tissue_geom(tissue_geom)
-            logging.info("Loaded tissue geom.")
-        
+
         model = getattr(self, model_name, None)
         if model is None:
             self.list_available_models()
-            logging.error(f"Model {model_name} not found in Diagnosis class.")
             raise ValueError(f"Model {model_name} not found in Diagnosis class.")
         model.load_model(replace=replace_model)
 
@@ -136,5 +122,4 @@ class Diagnosis(Slicer):
         self.set_slice_key(slice_key=model.model_name)
         dataloader = self.get_inference_dataloader(**kwargs)
 
-        logging.info(f"Starting inference for model: {model_name}")
         model.infer(dataloader)
---
+++ toolkit/compath/qc/qc_models_v1/qc_models_v1.py
@@ -0,0 +1,329 @@
+from pathlib import Path
+
+script_dir = Path(__file__).parent
+weights_dir = script_dir / "weights"
+
+import geojson
+from tqdm.auto import tqdm
+
+import torch
+
+from toolkit.system.storage.load import h5, save_geojson
+from toolkit.vision.image_tools import get_rgb_colors, get_cmap
+
+from toolkit.geometry.cv2_tools import get_contours, get_shapely_poly
+from toolkit.geometry.torch_tools import median_blur
+from toolkit.geometry.shapely_tools import MultiPolygon, loads, geom_to_geojson
+
+from toolkit.vision.deep_learning.torchmodel import Model
+
+
+class _QCModel(Model):
+    def __init__(
+        self,
+        gpu_id: int = 0,
+        device_type: str = "gpu",
+        dataparallel: bool = False,
+        dataparallel_device_ids: list[int] = None,
+    ):
+        Model.__init__(self, gpu_id, device_type, dataparallel, dataparallel_device_ids)
+
+    def infer(
+        self,
+        dataloader,
+        show_infer_progress=True,
+        show_merge_preds_progress=True,
+        show_process_merged_preds_progress=True,
+    ):
+        if self.model is None:
+            raise ValueError("No model loaded, load a model first.")
+        self.model.eval()
+
+        self._set_inference_params(dataloader)
+        self._pred_dicts = []
+
+        if show_infer_progress:
+            iterator = tqdm(self._dataloader, desc=f"Running {self.model_name} model")
+        else:
+            iterator = self._dataloader
+
+        with torch.inference_mode():
+            with torch.autocast(
+                device_type=self.device.type,
+                dtype=torch.float16 if self.device.type == "cuda" else torch.float32,
+            ):
+                for batch, tissue_masks in iterator:
+                    batch = batch.to(self.device) - 0.5
+                    preds = self.model(batch)
+                    preds = torch.argmax(preds, 1)
+
+                    tissue_masks = tissue_masks.to(self.device)
+                    preds *= tissue_masks
+                    del tissue_masks
+
+                    preds = preds.float().unsqueeze(1)
+                    preds = median_blur(preds, 15)
+                    preds = preds.squeeze(1).to(torch.uint8).to("cpu")
+
+                    for pred in preds:
+                        self._pred_dicts.append(self._process_pred(pred))
+
+                if self.device.type == "cuda":
+                    torch.cuda.empty_cache()
+
+        self._merge_preds(show_merge_preds_progress=show_merge_preds_progress)
+        self._process_merged_preds(
+            show_process_merged_preds_progress=show_process_merged_preds_progress
+        )
+        self._convert_to_geojson()
+
+    def _set_inference_params(self, dataloader):
+        self._dataloader = dataloader
+        self._params = self._dataloader.dataset.params
+        self._coordinates = [
+            coordinate for coordinate, _ in self._dataloader.dataset.coordinates
+        ]
+        self.h5_path = Path(
+            f"results/{self._dataloader.dataset.wsi_name.stem}/qc/h5/{self.model_name}.h5"
+        )
+        self.geojson_path = Path(
+            f"results/{self._dataloader.dataset.wsi_name.stem}/qc/geojson/{self.model_name}.geojson"
+        )
+
+        self.h5_path.parent.mkdir(parents=True, exist_ok=True)
+        self.geojson_path.parent.mkdir(parents=True, exist_ok=True)
+
+    def _process_pred(self, pred):
+        shift_dims = self._params["shift_dims"]
+        pred_sliced = pred[
+            shift_dims[0] : -shift_dims[0], shift_dims[1] : -shift_dims[1]
+        ]
+
+        pred_dict = {}
+        for key, value in self.class_map.items():
+            if key == "bg":
+                continue
+            class_mask = (pred_sliced == value).to(torch.uint8).numpy()
+            contours, hierarchy = get_contours(class_mask)
+            pred_dict[key] = [contours, hierarchy]
+
+        pred_sliced = pred_sliced.numpy()
+        pred_sliced[pred_sliced != 0] = 1
+        contours, hierarchy = get_contours(pred_sliced)
+        pred_dict["combined"] = [contours, hierarchy]
+
+        return pred_dict
+
+    def _merge_preds(self, show_merge_preds_progress):
+        assert len(self._coordinates) == len(
+            self._pred_dicts
+        ), "number of coordinates and predictions are unequal, can't merge back predictions together."
+
+        self._processed_pred_dict = {}
+        shift_dims = self._params["shift_dims"]
+        scale_factor = self._params["factor1"]
+
+        if show_merge_preds_progress:
+            iterator = tqdm(
+                zip(self._coordinates, self._pred_dicts),
+                total=len(self._pred_dicts),
+                desc="Merging predictions",
+            )
+        else:
+            iterator = zip(self._coordinates, self._pred_dicts)
+
+        for (x, y), pred_dict in iterator:
+            for key, (contours, hierarchy) in pred_dict.items():
+                if len(contours) == 0:
+                    continue
+                polys = get_shapely_poly(
+                    contours,
+                    hierarchy,
+                    scale_factor=scale_factor,
+                    shift_x=x + int(shift_dims[0] * scale_factor),
+                    shift_y=y + int(shift_dims[1] * scale_factor),
+                )
+                if key in self._processed_pred_dict:
+                    self._processed_pred_dict[key].extend(polys)
+                else:
+                    self._processed_pred_dict[key] = []
+                    self._processed_pred_dict[key].extend(polys)
+
+    def _process_merged_preds(self, show_process_merged_preds_progress):
+        wkt_dict = {}
+
+        if show_process_merged_preds_progress:
+            iterator = tqdm(
+                self._processed_pred_dict.items(), desc="Processing predictions"
+            )
+        else:
+            iterator = self._processed_pred_dict.items()
+
+        for key, value in iterator:
+            if show_process_merged_preds_progress:
+                iterator.set_description(f"Processing {key.capitalize()}")
+            wkt_dict[key] = MultiPolygon(value).buffer(0).wkt
+
+        h5.save_wkt_dict(wkt_dict, self.h5_path)
+
+    def _convert_to_geojson(self):
+        wkt_dict = h5.load_wkt_dict(self.h5_path)
+        colors = get_rgb_colors(len(wkt_dict), cmap=get_cmap(6))
+        geojson_features = []
+
+        for idx, dict_item in enumerate(wkt_dict.items()):
+            key, value = dict_item
+            geojson_feature = geom_to_geojson(loads(value))
+            geojson_feature["properties"] = {
+                "objectType": "annotation",
+                "name": key,
+                "color": colors[idx],
+            }
+            geojson_features.append(geojson_feature)
+        geojson_feature_collection = geojson.FeatureCollection(geojson_features)
+        save_geojson(
+            geojson_feature_collection,
+            self.geojson_path,
+        )
+
+
+class _TissueModelV1(_QCModel):
+    def __init__(
+        self,
+        gpu_id: int = 0,
+        patch_size: int = 1024,
+        device_type: str = "gpu",
+        dataparallel: bool = False,
+        dataparallel_device_ids: list[int] = None,
+    ):
+        _QCModel.__init__(
+            self,
+            gpu_id=gpu_id,
+            device_type=device_type,
+            dataparallel=dataparallel,
+            dataparallel_device_ids=dataparallel_device_ids,
+        )
+
+        self.model_name = "tissue_model_v1"
+        self.state_dict_path = Path(f"{weights_dir}/qc_models_v1/tissue_model_v1.pt")
+        self.class_map = {"bg": 0, "adipose": 1, "non_adipose": 2}
+        self.mpp = 4
+
+        self.patch_size = patch_size
+        self.overlap_size = int(self.patch_size * 0.0625)
+        self.context_size = self.overlap_size
+
+        self._model_class = "smp"
+        self._architecture = "UnetPlusPlus"
+        self._encoder_name = "resnet18"
+        self._encoder_weights = "imagenet"
+        self._in_channels = 3
+        self._classes = 3
+
+
+class _FoldsModelV1(_QCModel):
+    def __init__(
+        self,
+        gpu_id: int = 0,
+        patch_size: int = 1024,
+        device_type: str = "gpu",
+        dataparallel: bool = False,
+        dataparallel_device_ids: list[int] = None,
+    ):
+        _QCModel.__init__(
+            self,
+            gpu_id=gpu_id,
+            device_type=device_type,
+            dataparallel=dataparallel,
+            dataparallel_device_ids=dataparallel_device_ids,
+        )
+
+        self.model_name = "folds_model_v1"
+        self.state_dict_path = Path(f"{weights_dir}/qc_models_v1/folds_model_v1.pt")
+        self.class_map = {"bg": 0, "fold": 1}
+        self.mpp = 2
+
+        self.patch_size = patch_size
+        self.overlap_size = int(self.patch_size * 0.0625)
+        self.context_size = self.overlap_size
+
+        self._model_class = "smp"
+        self._architecture = "UnetPlusPlus"
+        self._encoder_name = "resnet18"
+        self._encoder_weights = "imagenet"
+        self._in_channels = 3
+        self._classes = 2
+
+
+class _FocusModelV1(_QCModel):
+    def __init__(
+        self,
+        gpu_id: int = 0,
+        patch_size: int = 1024,
+        device_type: str = "gpu",
+        dataparallel: bool = False,
+        dataparallel_device_ids: list[int] = None,
+    ):
+        _QCModel.__init__(
+            self,
+            gpu_id=gpu_id,
+            device_type=device_type,
+            dataparallel=dataparallel,
+            dataparallel_device_ids=dataparallel_device_ids,
+        )
+
+        self.model_name = "focus_model_v1"
+        self.state_dict_path = Path(f"{weights_dir}/qc_models_v1/focus_model_v1.pt")
+        self.class_map = {
+            "bg": 0,
+            "level_1": 1,
+            "level_2": 2,
+            "level_3": 3,
+            "level_4": 4,
+        }
+        self.mpp = 2
+
+        self.patch_size = patch_size
+        self.overlap_size = int(self.patch_size * 0.0625)
+        self.context_size = self.overlap_size
+
+        self._model_class = "smp"
+        self._architecture = "UnetPlusPlus"
+        self._encoder_name = "resnet18"
+        self._encoder_weights = "imagenet"
+        self._in_channels = 3
+        self._classes = 5
+
+
+class _PenModelV1(_QCModel):
+    def __init__(
+        self,
+        gpu_id: int = 0,
+        patch_size: int = 512,
+        device_type: str = "gpu",
+        dataparallel: bool = False,
+        dataparallel_device_ids: list[int] = None,
+    ):
+        _QCModel.__init__(
+            self,
+            gpu_id=gpu_id,
+            device_type=device_type,
+            dataparallel=dataparallel,
+            dataparallel_device_ids=dataparallel_device_ids,
+        )
+
+        self.model_name = "pen_model_v1"
+        self.state_dict_path = Path(f"{weights_dir}/qc_models_v1/pen_model_v1.pt")
+        self.class_map = {"bg": 0, "pen_mark": 1}
+        self.mpp = 16
+
+        self.patch_size = patch_size
+        self.overlap_size = int(self.patch_size * 0.0625)
+        self.context_size = self.overlap_size
+
+        self._model_class = "smp"
+        self._architecture = "UnetPlusPlus"
+        self._encoder_name = "resnet34"
+        self._encoder_weights = "imagenet"
+        self._in_channels = 3
+        self._classes = 2
